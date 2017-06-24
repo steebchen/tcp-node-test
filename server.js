5b251209df8553dc78846b1c16b7b1d954077565
Luca Steeb <contact@luca-steeb.com>
2017-06-24 19:27:17 +0200

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+var net = require('net')
+
+var server = net.createServer()
+
+server.on('connection', function (socket) {
+  console.log('ein client hat sich verbunden')
+
+  socket.on('data', function (data) {
+    console.log('empfangene daten vom client:', data.toString())
+  })
+})
+
+server.listen({ host: '127.0.0.1', port: 8000 }, function () {
+  console.log('warte auf verbindungen auf port', server.address())
+})
